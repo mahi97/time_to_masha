14b18df29c93f0cd7c03af38316e33aa09b72e72
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2024-08-28 16:19:13 +0900

    final2

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ function updateCountdown() {
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    const milliseconds = Math.floor((distance % 1000));
+    // const milliseconds = Math.floor((distance % 1000));
 
     timeElements.days.textContent = String(days).padStart(2, '0');
     timeElements.hours.textContent = String(hours).padStart(2, '0');
     timeElements.minutes.textContent = String(minutes).padStart(2, '0');
     timeElements.seconds.textContent = String(seconds).padStart(2, '0');
-    timeElements.milliseconds.textContent = String(milliseconds).padStart(3, '0');
+    // timeElements.milliseconds.textContent = String(milliseconds).padStart(3, '0');
 
     leftProgress.style.width = `${progressPosition}%`;
     rightProgress.style.width = `${progressPosition}%`;
