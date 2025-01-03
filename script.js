838d2566ba4bd5e284f31191d98dde2f532362f0
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2025-01-04 06:37:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const girlImage = document.getElementById('girl');
 const backgroundContainer = document.getElementById('background-images-container');
 
 // Convert KST to UTC
-let startDate = new Date(Date.UTC(2024, 6, 29, 22, 30, 0)).getTime(); // 1st August 2024, 00:00:00 KST -> 31st July 2024, 15:00:00 UTC
-let targetDate = new Date(Date.UTC(2024, 8, 4, 7, 0, 0)).getTime(); // 4th September 2024, 00:00:00 KST -> 3rd September 2024, 15:00:00 UTC
+let startDate = new Date(Date.UTC(2025, 1, 3, 23, 30, 0)).getTime(); // 1st August 2024, 00:00:00 KST -> 31st July 2024, 15:00:00 UTC
+let targetDate = new Date(Date.UTC(2025, 1, 12, 01, 30, 0)).getTime(); // 4th September 2024, 00:00:00 KST -> 3rd September 2024, 15:00:00 UTC
 let speed = 1;
 let flag = true;
 function updateCountdown() {
